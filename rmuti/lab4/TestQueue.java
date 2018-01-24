531b9901f03db4df7fac20ebc5f8e25449ffff1e
59332110321-5 <phontee2540@gmail.com>
2018-01-24 14:08:35 +0700

    ddd

---
+++ rmuti/lab4/TestQueue.java
@@ -0,0 +1,38 @@
+package rmuti.lab4;
+
+import rmuti.lab4.ArrayListQueue;
+
+public class TestQueue {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		ArrayListQueue queue = new ArrayListQueue();
+
+		queue.enqueque("macos");
+
+		queue.enqueque("linux");
+
+		queue.enqueque("windows");
+
+		System.out.println(queue);
+
+		
+
+		String data = (String)queue.dequeue();
+
+		System.out.println(queue);
+
+
+
+	}
+
+
+
+}
+
+
+
